6898bb52a140fd3efd25d8ec9264b37e53a94667
KamranMdShaikh <kamranmdshaikh@gmail.com>
2025-05-02 15:32:04 +0600

    connection set up using SSH

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 print("welcome back mr. data engineer")
+print("\nchacking connection using PAT")
 
 print("\n---------------------------\nI am here in GitHub")
+print("\nchaking connection using SSH")
+
